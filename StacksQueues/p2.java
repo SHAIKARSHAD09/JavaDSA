8aaf63f591fc436304ada3e2489c226312dd82fb
SHAIK <shaikarshad090909@gmail.com>
2025-04-07 21:09:05 +0530

    commit

--- StacksQueues/p2.java
+++ StacksQueues/p2.java
@@ -1,12 +1,14 @@
 package StacksQueues;
 
 import java.util.*;
-public class p2{
-    public static void main(String[] args){
+
+public class p2 {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        Stack s = new Stack();
-        for(int i=0; i<n; i++){
+        StackUsingTwoQueues s = new StackUsingTwoQueues();
+
+        for (int i = 0; i < n; i++) {
             s.push(sc.nextInt());
         }
 
@@ -17,40 +19,42 @@ public class p2{
     }
 }
 
-class Stack{
+class StackUsingTwoQueues {
     Queue<Integer> q1 = new LinkedList<>();
     Queue<Integer> q2 = new LinkedList<>();
 
-        void push(int x){
-            q1.offer(x);
+    // Push operation
+    void push(int x) {
+        q2.offer(x); // Add the new element to q2
+        while (!q1.isEmpty()) { // Transfer all elements from q1 to q2
+            q2.offer(q1.poll());
         }
+        // Swap references of q1 and q2
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+    }
 
-
-    int pop(){
+    // Pop operation
+    int pop() {
         if (q1.isEmpty()) {
             System.out.println("Stack is empty!");
             return -1;
         }
+        return q1.poll();
+    }
 
-        while (q1.size() > 1) {
-            q2.add(q1.poll());
+    // Peek operation
+    int peek() {
+        if (q1.isEmpty()) {
+            System.out.println("Stack is empty!");
+            return -1;
         }
-
-        int value = q1.poll();
-        
-        // Swap references of q1 and q2
-        Queue<Integer> temp = q1;
-        q1 = q2;
-		 q2 = temp;
-        
-        return value;
-    }
-
-    int size(){
-        return q1.size();
-    }
-
-    int peek(){
         return q1.peek();
+    }
+
+    // Size of the stack
+    int size() {
+        return q1.size();
     }
 }
